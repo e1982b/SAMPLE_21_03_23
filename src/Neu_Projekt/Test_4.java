2df092b7fa60413e5f45d0bf1033a1aeacfba98e
e1982b <e1982b@gmail.com>
2023-03-21 10:03:06 +0100

    New Formula added from Emre Branch in Test_4

--- src/Neu_Projekt/Test_4.java
+++ src/Neu_Projekt/Test_4.java
@@ -4,6 +4,7 @@ public class Test_4 {
     public static void main(String[] args) {
         int x = 10, y = 5, z = 19;
         System.out.println(x + y * z);
+        System.out.println(x * y - z);
         System.out.println(x * y * z);
         System.out.println(x * y + z);
     }
