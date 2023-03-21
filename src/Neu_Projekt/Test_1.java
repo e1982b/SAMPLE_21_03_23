44917d30ac9a91ede440cdeffc10609110e266ca
Emre BALCI <116108168+e1982b@users.noreply.github.com>
2023-03-21 09:52:55 +0100

    Update Test_1.java
    Added from Github

--- src/Neu_Projekt/Test_1.java
+++ src/Neu_Projekt/Test_1.java
@@ -4,5 +4,7 @@ public class Test_1 {
     public static void main(String[] args) {
         System.out.println("Hallochen");
         System.out.println("gut aber nicht schön");
+        // This line is added from GitHub
+        // Please update this file from IntelliJ
     }
 }
